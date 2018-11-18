74603c9ce36464fb7742f78c3bccb6cd535fae3a
Travis Pomeroy <travis.pomeroy84@gmail.com>
2018-11-17 19:54:49 -0500

    Small changes to app component.  Inlined the template.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
 
 @Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'apm-root',
+  template: `<div>
+    <h1>{{pageTitle}}</h1>
+    <div>My first component</div>
+  </div>`
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'Acme Product Management';
 }
